ca6415ff217daeb2edda4722d5fd36993b182abe
Jaxon Potter <jaxonpotter@gmail.com>
2020-01-23 15:50:26 -0700

    second commit

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/sum', (req, res) => {
-    const a = req.query.a;
-    const b = req.query.b;
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
     const c = a + b;
     //const greeting = `The sum of a and b is ${c}`
     if(!a) {
@@ -35,16 +35,37 @@ app.get('/cipher', (req, res) => {
 
 app.get('/lotto', (req, res) => {
     const arr = req.query.arr;
-    const computerArr = [];
-    const matches = 0;
-    const commonOccurences = computerArr
+    let computerArr = [];
+    let matches = 0;
     for(let i=0; i<=6; i++) {
         const number = Math.floor(Math.random() * 20);
         computerArr.push(number);
     }
+
+    for(let a=0; a<arr.length; a++){
+        for(let b=0; b<computerArr.length; b++){
+            if(parseInt(arr[a]) == parseInt(computerArr[b])){
+                matches = matches + 1;
+            }
+        }
+    }
+    if(matches == 6) {
+        res.send('Wow! Unbelievable! You could have won the mega millions!')
+    }
+    if(matches == 5) {
+        res.send('Congratulations! you win $100!')
+    }
+    if (matches == 4) {
+        res.send('Congratulations! You win a free ticket')
+    }
+    if (matches < 4) {
+        res.send('Sorry, you lose.')
+    }
+    
     res.send(`array is ${arr[0]}, ${arr[1]}, ${arr[2]}, ${arr[3]}, ${arr[4]}, ${arr[5]}
-        computer array is ${computerArr[0]}, ${computerArr[1]}, ${computerArr[2]}, ${computerArr[3]}, ${computerArr[4]}, ${computerArr[5]}
-        commonOccurences is ${commonOccurences}`)
+    computer array is ${computerArr[0]}, ${computerArr[1]}, ${computerArr[2]}, ${computerArr[3]}, ${computerArr[4]}, ${computerArr[5]}
+    commonOccurences is ${matches} common: ${common}`)
+    
 })
 
 app.listen(8000, () => {
